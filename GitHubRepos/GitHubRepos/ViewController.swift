164e5ed109a4334a4369a98d01ade1c2ea446bf3
Sreenath Bollam <sbollam@jazeerapaints.com-at-238527534465>
2019-11-27 10:51:34 +0530

    Created project and added required libraries

---
+++ GitHubRepos/GitHubRepos/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  GitHubRepos
+//
+//  Created by Sreenath reddy Bollam on 27/11/19.
+//  Copyright © 2019 SreenathReddy. All rights reserved.
+//
+
+import UIKit
+
+class TrendingReposViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ GitHubRepos/README.md
@@ -0,0 +1,8 @@
+# GitHubRepos ReadMe.md
+
+## Libraries used
+The following libraries have been used with CocoaPods as library dependency
+* Alamofire:
+      Networking library
+* Cosmos:
+      Cosmos, is a star rating control for iOS built in Swift
